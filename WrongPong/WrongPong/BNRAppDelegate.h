c8f36c1d601ea74b4005eca659a27f814b7e5889
Jonathan Blocksom <jblocksom@bignerdranch.com>
2013-09-18 12:17:23 -0400

    Adds WrongPoing demo for UIDynamics

---
+++ WrongPong/WrongPong/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  WrongPong
+//
+//  Created by Jonathan Blocksom on 9/16/13.
+//  Copyright (c) 2013 Jonathan Blocksom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WrongPong/WrongPong/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WrongPong
+//
+//  Created by Jonathan Blocksom on 9/16/13.
+//  Copyright (c) 2013 Jonathan Blocksom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BNRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
+    }
+}
