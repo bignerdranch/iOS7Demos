930301899f9edc51cf822eb01f5e4fa57881b5c8
Stephen Christopher <random4444@gmail.com>
2014-02-13 07:16:13 -0800

    change class of gray screen so class check in nav-delegate works.

---
+++ Collections/BNRGrayScreenVC.h
@@ -0,0 +1,13 @@
+//
+//  BNRGrayScreenVC.h
+//  Collections
+//
+//  Created by Stephen Christopher on 2/13/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRGrayScreenVC : UIViewController
+
+@end
---
+++ Collections/BNRGrayScreenVC.m
@@ -0,0 +1,38 @@
+//
+//  BNRGrayScreenVC.m
+//  Collections
+//
+//  Created by Stephen Christopher on 2/13/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import "BNRGrayScreenVC.h"
+
+@interface BNRGrayScreenVC ()
+
+@end
+
+@implementation BNRGrayScreenVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
